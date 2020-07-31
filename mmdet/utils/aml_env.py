8caeb145f770eab096831b5c991fa000043ea0d6
hust-nj <2402224349@qq.com>
2020-07-31 11:26:32 +0800

    use first ip

--- mmdet/utils/aml_env.py
+++ mmdet/utils/aml_env.py
@@ -5,7 +5,7 @@ def get_aml_master_ip(master_name=None):
     if 'AZ_BATCHAI_JOB_MASTER_NODE_IP' in os.environ:
         return os.environ['AZ_BATCHAI_JOB_MASTER_NODE_IP']
     elif 'OMPI_MCA_orte_local_daemon_uri' in os.environ:
-        return os.environ['OMPI_MCA_orte_local_daemon_uri'].split('/')[-1].split(':')[0].split(',')[-1]
+        return os.environ['OMPI_MCA_orte_local_daemon_uri'].split('/')[-1].split(':')[0].split(',')[0] # maybe multiple ip
         # DLTS_SD_ps0_IP
     else:
         print("ENVIRON:", os.environ)
