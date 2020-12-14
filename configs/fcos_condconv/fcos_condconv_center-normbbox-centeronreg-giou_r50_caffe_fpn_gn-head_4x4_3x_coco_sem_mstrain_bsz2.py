e80ef60fe3665b7744022de4725be8a53e773cc0
hust-nj <2402224349@qq.com>
2020-12-14 23:31:01 +0800

    add multi-scale train 3x

---
+++ configs/fcos_condconv/fcos_condconv_center-normbbox-centeronreg-giou_r50_caffe_fpn_gn-head_4x4_3x_coco_sem_mstrain_bsz2.py
@@ -0,0 +1,59 @@
+_base_= 'fcos_condconv_r50_caffe_fpn_gn-head_4x4_1x_coco.py'
+
+model = dict(
+    pretrained='open-mmlab://detectron2/resnet50_caffe',
+    bbox_head=dict(
+        norm_on_bbox=True,
+        centerness_on_reg=True,
+        dcn_on_last_conv=False,
+        center_sampling=True,
+        conv_bias=True,
+        loss_bbox=dict(type='GIoULoss', loss_weight=1.0),
+        mask_head=dict(branch_cfg=dict(semantic_loss_on=True))))
+# training and testing settings
+test_cfg = dict(nms=dict(type='nms', iou_threshold=0.6))
+
+# dataset settings
+img_norm_cfg = dict(
+    mean=[103.530, 116.280, 123.675], std=[1.0, 1.0, 1.0], to_rgb=False)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(type='Resize',
+        img_scale=[(1333, 640), (1333, 672), (1333, 704), (1333, 736), (1333, 768), (1333, 800)],
+        multiscale_mode='value',
+        keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(1333, 800),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+optimizer_config = dict(_delete_=True, grad_clip=None)
+
+lr_config = dict(warmup='linear')
+
+# learning policy
+lr_config = dict(step=[28, 34])
+total_epochs = 36
