0501bdf54fc62a04c44b241829af9a8397c45ca9
hust-nj <2402224349@qq.com>
2020-12-19 19:45:18 +0800

    change aml master ip

--- mmdet/utils/aml_env.py
+++ mmdet/utils/aml_env.py
@@ -2,7 +2,9 @@ import os
 
 
 def get_aml_master_ip(master_name=None):
-    if 'AZ_BATCHAI_JOB_MASTER_NODE_IP' in os.environ:
+    if 'MASTER_ADDR' in os.environ:
+        return os.environ['MASTER_ADDR']
+    elif 'AZ_BATCHAI_JOB_MASTER_NODE_IP' in os.environ:
         return os.environ['AZ_BATCHAI_JOB_MASTER_NODE_IP']
     elif 'OMPI_MCA_orte_local_daemon_uri' in os.environ:
         return os.environ['OMPI_MCA_orte_local_daemon_uri'].split('/')[-1].split(':')[0].split(',')[0] # maybe multiple ip
